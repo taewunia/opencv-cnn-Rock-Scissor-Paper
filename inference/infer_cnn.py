8a8b1cdf530dbf14ccba8d25262bce87d0f5a913
taewunia <glipon470@gmail.com>
2026-02-16 00:00:25 +0900

    file add

--- inference/infer_cnn.py
+++ inference/infer_cnn.py
@@ -4,8 +4,8 @@ import torch
 from models.cnn_model import CNN
 from utils.camera import camera
 device = torch.device(DEVICE)
-model = torch.load(CNN_MODEL_PATH, map_location=device)
+model = torch.load(CNN_MODEL_PATH, map_location=device, weight_only=False)
 
 with torch.no_grad():
     model.eval()
-    camera(model,CAP, TEST_PRE_PROCESS, device, CLASSES) #model, cap, pre_process, device, classes
+    camera(model,CAP, TEST_PRE_PROCESS, device, CLASSES)
